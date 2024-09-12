790067a58a182dc8e8473843cee332a10f603e6e
waqar864 <waqarkhan864@gmail.com>
2024-09-13 03:39:42 +0500

    conversions

---
+++ 01_basics/03_conversionOprations.js
@@ -0,0 +1,16 @@
+let score = "33";
+
+// console.log(typeof score);
+
+// let valueofNumber = Number(score);
+// console.log(valueofNumber);
+
+// "33" => 33
+// "33acb" => NAN
+// true => 1
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
