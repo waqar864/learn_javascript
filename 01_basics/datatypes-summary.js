f550204c79d8886dfb8d9b7a47f7c4d144c55361
waqar864 <waqarkhan864@gmail.com>
2024-10-02 03:24:57 +0500

    dataype summery

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+//premative
+
+
+//7 types : String, Number , Boolean, Null , Undefined ,symbol,Bigint
+
+const score = 100;
+const ScoreValue = 100.3;
+
+const isLoggedin = false;
+const OutsideTemp = null;
+let userEmail = undefined;
+
+const id = Symbol('123');
+const bigNumber = 23423432445342432n;
+
+//Refrence (Non Premative)
+
+// Array , Objects , Funvtions
+
+const heroes = ['batman','spiderman','superman'];
+
+
+    let myobj = {
+    name: "waqar",
+    age: "22",
+
+        }
+
+const myFunction = function(){
+    console.log("Hello World");
+}
