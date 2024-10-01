44d27187bfcbede8c712d40fb4e6561c89e808f8
waqar864 <waqarkhan864@gmail.com>
2024-10-02 01:30:48 +0500

    conversion of data types

--- 01_basics/03_conversionOprations.js
+++ 01_basics/03_conversionOprations.js
@@ -13,4 +13,23 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+/*------------------oprations------------------*/
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// ++ , -- , += , -= , *= , /= , %=
+
+
+let str1 = "hello";
+let str2 = "waqar";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+
