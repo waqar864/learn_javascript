679db645006f1a04d6f8417517f9d3fa9dd68235
waqar864 <waqarkhan864@gmail.com>
2024-10-03 16:10:12 +0500

    gradiant picker

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,1 +1,16 @@
 const myArr = [0,1,2,3,4,5]
+const myHeroes = ["batman","spiderman","superman"]
+
+//Array methods
+
+//for adding the element in array 
+
+// myArr.push(6);
+
+
+// myArr.pop();
+// console.log(myArr);
+
+const newArr = myArr.join();
+console.log(myArr);
+console.log(newArr);
